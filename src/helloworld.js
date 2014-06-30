a08d8a5b58eed0f408fc72b60abe824968eb7303
Adam K Dean <adamkdean@googlemail.com>
2014-06-30 16:16:45 +0100

    Added initial code to project

---
+++ src/helloworld.js
@@ -0,0 +1,13 @@
+var http = require('http'),
+    funkyname = require('funkyname.js');
+
+var name = funkyname.getFunkyName();
+var port = 8000;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('This instance is called ' + name + '\n');
+}).listen(port);
+
+console.log('This instance has been named ' + name);
+console.log('Listening on port ' + port)
