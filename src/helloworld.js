a35676bee62ab5c78dd971834fe3aac811e9f17a
Adam K Dean <adamkdean@googlemail.com>
2014-07-01 14:53:08 +0100

    Added a slash. Should definitely have this slash.

--- src/helloworld.js
+++ src/helloworld.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8000;
 http.createServer(function (req, res) {
     console.log('url', req.url);
     var content = 'This instance is called ' + name + '\n';
-    if (req.url == '/loaderio-87da3f91b6285d498fb44dbf02d42d4f') {
+    if (req.url == '/loaderio-87da3f91b6285d498fb44dbf02d42d4f/') {
         content = 'loaderio-87da3f91b6285d498fb44dbf02d42d4f';
     }
     res.writeHead(200, {'Content-Type': 'text/plain'});
