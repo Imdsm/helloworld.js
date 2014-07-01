650681efc0ad06a47b3c9d1ec146b5afaf66393a
Adam K Dean <adamkdean@googlemail.com>
2014-07-01 14:47:24 +0100

    Added loaderio load testing request handler

--- src/helloworld.js
+++ src/helloworld.js
@@ -2,12 +2,16 @@ var http = require('http'),
     funkyname = require('funkyname.js');
 
 var name = funkyname.getFunkyName();
-
 var port = process.env.PORT || 8000;
 
 http.createServer(function (req, res) {
+    console.log('url', req.url);
+    var content = 'This instance is called ' + name + '\n';
+    if (req.url == '/loaderio-87da3f91b6285d498fb44dbf02d42d4f') {
+        content = 'loaderio-87da3f91b6285d498fb44dbf02d42d4f';
+    }
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('This instance is called ' + name + '\n');
+    res.end(content);
 }).listen(port);
 
 console.log('This instance has been named ' + name);
